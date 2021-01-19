7aad158133317e6baa1bde7891872d0313b006a3
FatmaMama <fatma_mama@hotmail.fr>
2021-01-19 18:10:36 +0100

    html css component nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,21 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {MatDialogModule} from '@angular/material/dialog';
+
+//components
 import { AppComponent } from './app.component';
-import { NavComponentComponent } from './Components/nav-component/nav-component.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { SearchComponent } from './Components/search/search.component';
 import { DeleteComponent } from './Components/delete/delete.component';
 import { AddComponent } from './Components/add/add.component';
 import { HomeComponent } from './Components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponentComponent,
     NavComponent,
     SearchComponent,
     DeleteComponent,
@@ -22,7 +24,9 @@ import { HomeComponent } from './Components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
